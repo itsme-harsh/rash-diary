93f093879bcceb7bfff9f60c803362034e85c1fc
itsme-harsh <harshdevadiya.ce20@sltiet.edu.in>
2024-09-15 13:20:45 +0530

    --config vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,14 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/base': {
-        target: 'https://rash-diary.onrender.com', // Replace with your backend server address
-        // target: 'http://localhost:3000', // Replace with your backend server address
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/base/, ''),
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/base': {
+  //       target: 'https://rash-diary.onrender.com', // Replace with your backend server address
+  //       // target: 'http://localhost:3000', // Replace with your backend server address
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/base/, ''),
+  //     },
+  //   },
+  // },
 });
